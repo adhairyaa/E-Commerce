8d835f707663c8533b22e25ae95b4bef909009c7
Dhairya <dhairyagulati370@gmail.com>
2023-01-31 13:43:26 +0530

    Add : Filter Context

---
+++ src/context/FilterProvider.js
@@ -0,0 +1,56 @@
+import { createContext, useContext, useReducer } from "react";
+
+const FilterContext = createContext();
+
+const handleDispatch = (state, action) => {
+  switch (action.type) {
+    case "LOW_TO_HIGH":
+      return (state = {
+        ...state,
+        sortBy: "LOW_TO_HIGH",
+      });
+    case "HIGH_TO_LOW":
+      return (state = { ...state, sortBy: "HIGH_TO_LOW" });
+    case "GENDER_FILTER":
+      return (state = {
+        ...state,
+        gender: state.gender.includes(action.payload)
+          ? state.gender.filter((gender) => gender !== action.payload)
+          : [...state.gender, action.payload],
+      });
+    case "SIZE_FILTER":
+      return (state = {
+        ...state,
+        size: state.size.includes(action.payload)
+          ? state.size.filter((size) => size !== action.payload)
+          : [...state.size, action.payload],
+      });
+    case "CLEAR_FILTER":
+      return (state = {
+        ...state,
+        size: [],
+        gender: [],
+        sortBy: null,
+      });
+
+    default:
+      return state;
+  }
+};
+export function FilterProvider({ children }) {
+  const [state, dispatch] = useReducer(handleDispatch, {
+    sortBy: null,
+    gender: [],
+    size: [],
+  });
+
+  return (
+    <FilterContext.Provider value={{ state, dispatch }}>
+      {children}
+    </FilterContext.Provider>
+  );
+}
+
+const useStateProvider = () => useContext(FilterContext);
+
+export { useStateProvider };
--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import { WishlistProvider } from "./context/WishlistContext";
 import { CartProvider } from "./context/CartProvider";
+import { FilterProvider } from "./context/FilterProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-    <CartProvider>
-      <WishlistProvider>
-        <App />
-      </WishlistProvider>
-    </CartProvider>
+      <FilterProvider>
+        <CartProvider>
+          <WishlistProvider>
+            <App />
+          </WishlistProvider>
+        </CartProvider>
+      </FilterProvider>
     </Router>
   </React.StrictMode>
 );
