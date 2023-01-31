71e5dbcce4866d04061641870927ec5f616fcdfe
Dhairya <dhairyagulati370@gmail.com>
2023-01-31 13:37:20 +0530

    Update : Filter component logic

---
+++ src/components/filter/Filter.jsx
@@ -0,0 +1,78 @@
+import { useStateProvider } from "../../context/StateProvider";
+import "./Filter.css";
+function Filter() {
+  const { dispatch } = useStateProvider();
+
+  return (
+    <div className="filterBar">
+      <div className="filterBar-container">
+        <div className="filterBar_title">
+          <h5>Filter</h5>
+          <button onClick={() => dispatch({ type: "CLEAR_FILTER" })}>
+            Clear
+          </button>
+        </div>
+        <div className="sort-by-filter">
+          <p>Sort-by</p>
+          <div>
+            <input
+              type="radio"
+              name="sort-by"
+              onClick={() => dispatch({ type: "LOW_TO_HIGH" })}
+            ></input>{" "}
+            low-to-high
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="sort-by"
+              onClick={() => dispatch({ type: "HIGH_TO_LOW" })}
+            ></input>{" "}
+            high-to-low
+          </div>
+        </div>
+        <div className="gender-filter">
+          <p>Gender</p>
+          <input
+            type="checkbox"
+            onClick={() => dispatch({ type: "GENDER_FILTER", payload: "men" })}
+          ></input>
+          Men
+          <input
+            type="checkbox"
+            onClick={() =>
+              dispatch({ type: "GENDER_FILTER", payload: "women" })
+            }
+          ></input>
+          Women
+        </div>
+        <div className="size-filter">
+          <p>Size</p>
+          <input
+            type="checkbox"
+            onClick={() => dispatch({ type: "SIZE_FILTER", payload: "L" })}
+          ></input>
+          L
+          <input
+            type="checkbox"
+            onClick={() => dispatch({ type: "SIZE_FILTER", payload: "XL" })}
+          ></input>
+          XL
+          <input
+            type="checkbox"
+            onClick={() => dispatch({ type: "SIZE_FILTER", payload: "S" })}
+          ></input>
+          S
+          <input
+            type="checkbox"
+            onClick={() => dispatch({ type: "SIZE_FILTER", payload: "M" })}
+          ></input>
+          M
+        </div>
+        <div className="brand-filter"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Filter;
