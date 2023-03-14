c16aa34b3a5205b3493ee1afe5480c6fd5d02f28
Dhairya <dhairyagulati370@gmail.com>
2023-03-14 17:58:45 +0530

    update : Home page CSS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,8 +9,16 @@ export function Home() {
             alt="HeroImage"
           ></img>
         </Link>
-        <div className="heroSectionText">40%OFF</div>
+
+        <div className="heroSectionText">
+          <div>40%OFF</div>
+          <Link to="/products">
+            {" "}
+            <button>Shop Now</button>
+          </Link>
+        </div>
       </div>
+      <h2 className="sectionHeading">Section Collection</h2>
 
       <div className="HomeSectionTiles">
         <Link to="/products">
@@ -40,6 +48,28 @@ export function Home() {
             <div className="tileText">Men's</div>
           </div>
         </Link>
+      </div>
+      <div className="footerSection">
+        <div className="footer">
+          <div className="footerCompanyDetails">
+            <span style={{ fontSize: "larger", fontWeight: "bolder" }}>
+              Company
+            </span>
+            <p>Find a location nearest you</p>
+            <p>+91 - 7575757576</p>
+            <p>hello@buye.com</p>
+          </div>
+          <div className="footerSiteDetails">
+            <span style={{ fontSize: "larger", fontWeight: "bolder" }}>
+              Information
+            </span>
+            <p>Products</p>
+            <p>My Cart</p>
+            <p>Wishlist</p>
+            <p>My Account</p>
+          </div>
+        </div>
+        <div className="footerLogo">Buye 2023</div>
       </div>
     </div>
   );
